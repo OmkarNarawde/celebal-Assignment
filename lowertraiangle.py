2f925aedf66bf5dc9a58204ec463f935008d433c
OmkarNarawde <omkarnarawde1@gmail.com>
2024-06-07 18:56:56 +0530

    assignment

---
+++ lowertraiangle.py
@@ -0,0 +1,9 @@
+#Lower Triangular
+def lower_traingle(n):
+    for i in range(1,n+1):
+        print('*' *i)
+
+n=5
+print("Lower traingular:")
+lower_traingle(n)
+        
---
+++ uppertraingular.py
@@ -0,0 +1,8 @@
+#upper traingular
+def upper_traingle(n):
+    for i in range(n,0,-1):
+        print('*' *i)
+
+n=5
+print("upper traingular :")
+upper_traingle(n)
